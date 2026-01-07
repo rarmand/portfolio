d498ae0db1724854370a6255c50824f1bee9f1e5
rarmand <holik.alek@gmail.com>
2026-01-07 15:21:40 +0100

    first try of tailwind and some text

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header>
+        <h1 className='text-4xl font-bold'>Aleksandra Holik</h1>
+        <h2 className='text-2xl font-semibold'>I am a creator, not a coder.</h2>
+      </header>
+      <main>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Hey, I am a young programmer, who loves creating and building
+          functional and beautiful websites.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
