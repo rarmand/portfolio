2d4c304b33cd53d6c8a57865507ab59a348a1bd0
rarmand <holik.alek@gmail.com>
2026-01-10 16:03:01 +0100

    fixed Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,30 @@
 function Header() {
   return (
-    <header className='sticky top-0'>
+    <header className='fixed top-0 left-0 right-0 bg-slate-50'>
       <div className='container mx-auto flex flex-row items-center justify-between py-8'>
         <h1 className='flex text-2xl'>Aleksandra Holik</h1>
-        <div className='flex flex-row items-center gap-8'>
-          <nav>
-            <ul className='flex flex-row gap-8'>
-              <li>
-                <a href='#about'>About me</a>
-              </li>
-              <li>
-                <a href='#skills'>Skills</a>
-              </li>
-              <li>
-                <a href='#experience'>Experience</a>
-              </li>
-              <li>
-                <a href='#education'>Education</a>
-              </li>
-              <li>
-                <a href='#projects'>Projects</a>
-              </li>
-              <li>
-                <a href='#contact'>Contact</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <nav className='flex flex-row items-center gap-8'>
+          <ul className='flex flex-row gap-8'>
+            <li>
+              <a href='#about'>About me</a>
+            </li>
+            <li>
+              <a href='#skills'>Skills</a>
+            </li>
+            <li>
+              <a href='#experience'>Experience</a>
+            </li>
+            <li>
+              <a href='#education'>Education</a>
+            </li>
+            <li>
+              <a href='#projects'>Projects</a>
+            </li>
+            <li>
+              <a href='#contact'>Contact</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
